22d7d7fea906fc23badb6bb5764188954e44fedc
Lily Garipova <lgaripova@momtrusted.com>
2013-08-04 18:12:38 -0700

    Adding pushBack, pushFront, last, << methods

--- Linkedlists.rb
+++ Linkedlists.rb
@@ -33,21 +33,20 @@ class List
   
   def initialize(length)
     prevNode=nil
-    first=nil
+    @first=nil
     length.times do |num|
       node=Node.new
-      first=node if first==nil
+      @first=node if @first==nil
       node.name="node: #{num}"
       prevNode.next=node if prevNode!=nil
       prevNode=node
     end
-    @first=first
   end
   
   def length
-    return 0 if first==nil
+    return 0 if @first==nil
     count=1
-    node=first 
+    node=@first 
     while node.next!=nil do 
       node=node.next
       count+=1
@@ -58,9 +57,9 @@ class List
   
   def to_s
     
-    return 'empty' if first==nil
-    node=first
-    string=first.to_s
+    return 'empty' if @first==nil
+    node=@first
+    string=@first.to_s
     
     while node.next!=nil do
       node=node.next
@@ -70,11 +69,55 @@ class List
     return string
   end
   
+  def last
+    return nil if @first==nil
+    node=@first
+    while node.next!=nil do 
+      node=node.next
+    end
+    return node
+  end
+  
+  def pushBack(node)
+    l=last 
+    if l==nil
+      @first=node 
+    else
+      l.next=node
+    end
+  end
+  
+  def <<(node)
+    pushBack(node)
+  end
+  
+  def pushFront(node)
+    if @first==nil
+      @first=node
+    else      
+      node.next=@first
+      @first=node
+    end  
+  end
 end
 
 
+
+
 myList=List.new(10)
-puts myList.length
+#puts myList.length
+#puts myList.last
+
+n=Node.new
+n.name="extra"
+
+#myList.pushBack(n)
+myList << n
+#puts myList
+
+n=Node.new
+n.name="extra front"
+myList.pushFront(n)
 puts myList
 
 
